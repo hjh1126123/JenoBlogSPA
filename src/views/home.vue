a8abf7d86f35656901d74abd9def12e055d291fa
Jena.Huang <945664458@qq.com>
2019-01-22 17:31:03 +0800

    Initial commit

---
+++ src/views/home.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <p>你好，我是SSR Home，多加一个字，FK，还是热更新</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home",
+        title () {
+           return "家"
+        },
+        data() {
+            return {}
+        },
+        methods: {},
+        props: {},
+        filters: {},
+        components: {},
+        computed: {},
+        mounted() {
+        },
+        beforeDestroy() {
+        },
+        beforeCreate() {
+        },
+        created() {
+        },
+        beforeMount() {
+        },
+        beforeUpdate() {
+        },
+        updated() {
+        },
+        destroyed() {
+        }
+    }
+</script>
+
+<style lang="stylus">
+
+</style>
